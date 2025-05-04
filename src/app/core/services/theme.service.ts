6bff9b6824ec90862ed2e09dbc9b40962bec6d10
Debid Rana Magar <linkwithdm@gmail.com>
2025-05-05 00:42:39 +0545

    feat: add theme switch functionality but not completed

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@angular/core';
+import {effect, Injectable, signal} from '@angular/core';
+
+interface Theme {
+  mode: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  public theme = signal<Theme>({
+    mode: 'dark'
+  });
 
-  constructor() { }
+  constructor() {
+    this.loadTheme();
+    effect(() => {
+      this.setConfig();
+    });
+  }
+  private loadTheme() {
+    const hour = new Date().getHours();
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      this.theme.set(JSON.parse(theme));
+    } else {
+      const theme = hour >= 18 || hour < 6 ? 'dark' : 'light';
+      this.theme.set({ mode: theme });
+    }
+  }
+  private setConfig() {
+    this.setThemeOnLocalStorage();
+    this.setThemeClass();
+    // this.setRTL();
+  }
+
+  private setThemeClass() {
+    document.querySelector('html')!.className = this.theme().mode;
+    // document
+    //   .querySelector('html')!
+    //   .setAttribute('data-theme', this.theme().color);
+  }
+
+  private setThemeOnLocalStorage() {
+    localStorage.setItem('theme', JSON.stringify(this.theme()));
+  }
+
+  public getTheme(): string {
+    return this.theme().mode || 'light';
+  }
+
+  public get isDark(): boolean {
+    return this.theme().mode == 'dark';
+  }
+
+  updateTheme() {
+    this.theme.update((theme) => {
+      const mode = !this.isDark ? 'dark' : 'light';
+      return { ...theme, mode: mode };
+    });
+  }
 }
