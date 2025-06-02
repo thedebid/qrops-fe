a643c6e61848081a20de6026e06c434b24719405
Debid Rana Magar <linkwithdm@gmail.com>
2025-06-02 09:17:09 +0545

    feat: add dropdown for restaurant selection

--- src/app/pages/dashboard/components/header/header.component.ts
+++ src/app/pages/dashboard/components/header/header.component.ts
@@ -7,12 +7,35 @@ import {
   LogOut,
   X,
   Menu,
+  ChevronDown,
 } from 'lucide-angular';
 import { LayoutService } from '../../../../core/services/layout.service';
-import {ThemeToggleComponent} from '../../../../shared/components/theme-toggle/theme-toggle.component';
+import { ThemeToggleComponent } from '../../../../shared/components/theme-toggle/theme-toggle.component';
+import { AsyncPipe, NgClass, NgForOf, NgIf, NgStyle } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { AppState } from '../../../../store/app.state';
+import { Store } from '@ngrx/store';
+import {
+  selectAllRestaurants,
+  selectSelectedRestaurant,
+} from '../../../../store/restaurant/restaurant.selectors';
+import {
+  loadRestaurants,
+  selectRestaurant,
+} from '../../../../store/restaurant/restaurant.actions';
+import { Restaurant } from '../../../../store/restaurant/restaurant.model';
 @Component({
   selector: 'app-header',
-  imports: [LucideAngularModule, RouterLink, ThemeToggleComponent],
+  imports: [
+    LucideAngularModule,
+    RouterLink,
+    ThemeToggleComponent,
+    NgStyle,
+    NgClass,
+    NgIf,
+    NgForOf,
+    AsyncPipe,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
@@ -22,6 +45,7 @@ export class HeaderComponent {
   readonly LogOutIcon = LogOut;
   readonly XIcon = X;
   readonly MenuIcon = Menu;
+  readonly ChevronDownIcon = ChevronDown;
 
   showUserMenu: boolean = false;
 
@@ -30,10 +54,37 @@ export class HeaderComponent {
 
     this.showUserMenu = !this.showUserMenu;
   }
-  constructor(public layoutService: LayoutService) {}
+  constructor(
+    public layoutService: LayoutService,
+    private store: Store<AppState>
+  ) {}
 
   toggleSidebar() {
     this.layoutService.toggleSidebar();
     console.log('Sidebar toggled:', this.layoutService.isSidebarOpen());
+  }
+
+  isDropdownOpen = false;
+
+  public restaurants$: Observable<any> = of([]);
+  public selectedRestaurant$: Observable<any> = of({});
+
+  ngOnInit() {
+    this.store.dispatch(loadRestaurants());
+    this.restaurants$ = this.store.select(selectAllRestaurants);
+    this.selectedRestaurant$ = this.store.select(selectSelectedRestaurant);
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectRestaurant(restaurant: Restaurant): void {
+    this.store.dispatch(selectRestaurant({ restaurant }));
+    this.isDropdownOpen = false;
+  }
+
+  viewAllRestaurants() {
+    this.isDropdownOpen = false;
   }
 }
